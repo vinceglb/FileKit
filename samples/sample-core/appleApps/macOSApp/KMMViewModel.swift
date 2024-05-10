0c201d0aab0b8accf976b31179559efcb2d2968d
Vincent Guillebaud <v.guillebaud@akawan.fr>
2024-05-10 11:26:00 +0200

    ♻️ Remove Skie and migrate samples to KMMViewModel

---
+++ samples/sample-core/appleApps/macOSApp/KMMViewModel.swift
@@ -0,0 +1,11 @@
+//
+//  KMMViewModel.swift
+//  macOSApp
+//
+//  Created by Vincent Guillebaud on 09/05/2024.
+//
+
+import KMMViewModelCore
+import SamplePickerKt
+
+extension Kmm_viewmodel_coreKMMViewModel: KMMViewModel { }
