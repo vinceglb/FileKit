fd97089559cf141af9c7872da7931276a8c45088
Griffin Sorrentino <griffinsorrentino@gmail.com>
2024-06-28 16:33:34 -0400

    #51 Don't init FileKit if in inspection (preview) mode

--- filekit-compose/src/androidMain/kotlin/io/github/vinceglb/filekit/compose/FileKitCompose.android.kt
+++ filekit-compose/src/androidMain/kotlin/io/github/vinceglb/filekit/compose/FileKitCompose.android.kt
@@ -5,11 +5,14 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.platform.LocalContext
 import io.github.vinceglb.filekit.core.FileKit
+import androidx.compose.ui.platform.LocalInspectionMode
 
 @Composable
 internal actual fun InitFileKit() {
-    val componentActivity = LocalContext.current as ComponentActivity
-    LaunchedEffect(Unit) {
-        FileKit.init(componentActivity)
+    if (!LocalInspectionMode.current) {
+        val componentActivity = LocalContext.current as ComponentActivity
+        LaunchedEffect(Unit) {
+            FileKit.init(componentActivity)
+        }
     }
 }
