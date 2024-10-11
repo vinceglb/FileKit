aa80e51422e4c838431beb424126eace12110dbf
Vincent Guillebaud <vincent.guillebaud31@gmail.com>
2024-10-12 00:04:56 +0200

    🚚 Fix filekit-coil files names

--- filekit-coil/src/jvmMain/kotlin/io/github/vinceglb/filekit/coil/FileKitCoil.jvm.kt
+++ filekit-coil/src/jvmMain/kotlin/io/github/vinceglb/filekit/coil/FileKitCoil.jvm.kt
@@ -1,8 +1,6 @@
 package io.github.vinceglb.filekit.coil
 
-import androidx.compose.runtime.Composable
 import io.github.vinceglb.filekit.PlatformFile
 
-
-
-
+public actual val PlatformFile.coilModel: Any
+    get() = file
--- filekit-coil/src/jvmMain/kotlin/io/github/vinceglb/filekit/coil/FileKitCoil.nonWeb.jvm.kt
+++
@@ -1,6 +0,0 @@
-package io.github.vinceglb.filekit.coil
-
-import io.github.vinceglb.filekit.PlatformFile
-
-public actual val PlatformFile.coilModel: Any
-    get() = file
