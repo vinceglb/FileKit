3396dd40d289538f7463751949515ef101eb157e
Vincent Guillebaud <vincent.guillebaud31@gmail.com>
2024-10-20 19:06:22 +0200

    🔥 Remove old PlatformInputStream files

--- filekit-core/src/nonWebMain/kotlin/io/github/vinceglb/filekit/PlatformInputStream.kt
+++
@@ -1,7 +0,0 @@
-package io.github.vinceglb.filekit
-
-public expect class PlatformInputStream : AutoCloseable {
-    public fun hasBytesAvailable(): Boolean
-    public suspend fun readInto(buffer: ByteArray, maxBytes: Int): Int
-    public override fun close()
-}
