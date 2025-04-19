37563000bab073e673fc2a774b49ee562c487ad0
Vincent Guillebaud <vincent.guillebaud31@gmail.com>
2025-04-20 00:02:34 +0200

    🍱 Add sample explorer icons

---
+++ samples/sample-file-explorer/composeApp/src/commonMain/kotlin/io/github/vinceglb/sample/explorer/icon/Folder.kt
@@ -0,0 +1,49 @@
+package io.github.vinceglb.sample.explorer.icon
+
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.SolidColor
+import androidx.compose.ui.graphics.StrokeCap
+import androidx.compose.ui.graphics.StrokeJoin
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.graphics.vector.path
+import androidx.compose.ui.unit.dp
+
+val ExplorerIcons.Folder: ImageVector
+    get() {
+        if (_Folder != null) {
+            return _Folder!!
+        }
+        _Folder = ImageVector.Builder(
+            name = "Folder",
+            defaultWidth = 24.dp,
+            defaultHeight = 24.dp,
+            viewportWidth = 24f,
+            viewportHeight = 24f
+        ).apply {
+            path(
+                stroke = SolidColor(Color(0xFF000000)),
+                strokeLineWidth = 2f,
+                strokeLineCap = StrokeCap.Round,
+                strokeLineJoin = StrokeJoin.Round
+            ) {
+                moveTo(20f, 20f)
+                arcToRelative(2f, 2f, 0f, isMoreThanHalf = false, isPositiveArc = false, 2f, -2f)
+                verticalLineTo(8f)
+                arcToRelative(2f, 2f, 0f, isMoreThanHalf = false, isPositiveArc = false, -2f, -2f)
+                horizontalLineToRelative(-7.9f)
+                arcToRelative(2f, 2f, 0f, isMoreThanHalf = false, isPositiveArc = true, -1.69f, -0.9f)
+                lineTo(9.6f, 3.9f)
+                arcTo(2f, 2f, 0f, isMoreThanHalf = false, isPositiveArc = false, 7.93f, 3f)
+                horizontalLineTo(4f)
+                arcToRelative(2f, 2f, 0f, isMoreThanHalf = false, isPositiveArc = false, -2f, 2f)
+                verticalLineToRelative(13f)
+                arcToRelative(2f, 2f, 0f, isMoreThanHalf = false, isPositiveArc = false, 2f, 2f)
+                close()
+            }
+        }.build()
+
+        return _Folder!!
+    }
+
+@Suppress("ObjectPropertyName")
+private var _Folder: ImageVector? = null
