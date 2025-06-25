37ba6f51fe5e3d76aa45540935f7242fe54cf39c
Vincent Guillebaud <vincent.guillebaud31@gmail.com>
2025-06-26 01:31:59 +0200

    🧑‍💻 Improve developer experience by adding new FileKitModes: SingleWithState and MultipleWithState

--- filekit-dialogs/src/commonMain/kotlin/io/github/vinceglb/filekit/dialogs/FileKitMode.kt
+++ filekit-dialogs/src/commonMain/kotlin/io/github/vinceglb/filekit/dialogs/FileKitMode.kt
@@ -1,29 +1,131 @@
 package io.github.vinceglb.filekit.dialogs
 
 import io.github.vinceglb.filekit.PlatformFile
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.last
+import kotlinx.coroutines.flow.mapNotNull
 
-public sealed class FileKitMode<Out> {
-    public abstract fun parseResult(value: List<PlatformFile>?): Out?
+public sealed class FileKitMode<PickerResult, ConsumedResult> {
+    internal abstract fun getPickerMode(): PickerMode
 
-    public data object Single : FileKitMode<PlatformFile>() {
-        override fun parseResult(value: List<PlatformFile>?): PlatformFile? {
-            return value?.firstOrNull()
+    internal abstract suspend fun parseResult(flow: Flow<FileKitPickerState<List<PlatformFile>>>): PickerResult
+
+    public abstract suspend fun consumeResult(
+        result: PickerResult,
+        onConsumed: (ConsumedResult) -> Unit
+    )
+
+    public data object Single : FileKitMode<PlatformFile?, PlatformFile?>() {
+        override fun getPickerMode(): PickerMode = PickerMode.Single
+        override suspend fun parseResult(flow: Flow<FileKitPickerState<List<PlatformFile>>>): PlatformFile? {
+            return flow.last().let {
+                when (it) {
+                    is FileKitPickerState.Completed -> it.result.firstOrNull()
+                    else -> null
+                }
+            }
+        }
+
+        override suspend fun consumeResult(
+            result: PlatformFile?,
+            onConsumed: (PlatformFile?) -> Unit
+        ) {
+            onConsumed(result)
         }
     }
 
-    /**
-     * @property maxItems sets the limit of how many items can be selected. NOTE: This is only
-     * supported by Android / iOS and only when picking media files, not any kind of file.
-     */
-    public data class Multiple(val maxItems: Int? = null) : FileKitMode<List<PlatformFile>>() {
+    public data class Multiple(val maxItems: Int? = null) :
+        FileKitMode<List<PlatformFile>?, List<PlatformFile>?>() {
+        override fun getPickerMode(): PickerMode = PickerMode.Multiple(maxItems)
+        override suspend fun parseResult(flow: Flow<FileKitPickerState<List<PlatformFile>>>): List<PlatformFile>? {
+            return flow.last().let {
+                when (it) {
+                    is FileKitPickerState.Completed -> it.result
+                    else -> null
+                }
+            }
+        }
+
+        override suspend fun consumeResult(
+            result: List<PlatformFile>?,
+            onConsumed: (List<PlatformFile>?) -> Unit
+        ) {
+            onConsumed(result)
+        }
+    }
+
+    public data object SingleWithState :
+        FileKitMode<Flow<FileKitPickerState<PlatformFile>>, FileKitPickerState<PlatformFile>>() {
+        override fun getPickerMode(): PickerMode = PickerMode.Single
+
+        override suspend fun parseResult(flow: Flow<FileKitPickerState<List<PlatformFile>>>): Flow<FileKitPickerState<PlatformFile>> {
+            return flow.mapNotNull {
+                when (it) {
+                    is FileKitPickerState.Cancelled -> FileKitPickerState.Cancelled
+                    is FileKitPickerState.Started -> FileKitPickerState.Started(
+                        total = it.total
+                    )
+
+                    is FileKitPickerState.Progress -> {
+                        it.processed.firstOrNull()?.let { file ->
+                            FileKitPickerState.Progress(
+                                processed = file,
+                                total = it.total
+                            )
+                        }
+                    }
+
+                    is FileKitPickerState.Completed -> {
+                        it.result.firstOrNull()?.let { file ->
+                            FileKitPickerState.Completed(result = file)
+                        }
+                    }
+                }
+            }
+        }
+
+        override suspend fun consumeResult(
+            result: Flow<FileKitPickerState<PlatformFile>>,
+            onConsumed: (FileKitPickerState<PlatformFile>) -> Unit
+        ) {
+            result.collect(onConsumed)
+        }
+    }
+
+    public data class MultipleWithState(val maxItems: Int? = null) :
+        FileKitMode<Flow<FileKitPickerState<List<PlatformFile>>>, FileKitPickerState<List<PlatformFile>>>() {
+        override fun getPickerMode(): PickerMode = PickerMode.Multiple(maxItems)
+
+        override suspend fun parseResult(flow: Flow<FileKitPickerState<List<PlatformFile>>>): Flow<FileKitPickerState<List<PlatformFile>>> {
+            return flow.mapNotNull {
+                when (it) {
+                    is FileKitPickerState.Cancelled -> FileKitPickerState.Cancelled
+                    is FileKitPickerState.Started -> FileKitPickerState.Started(total = it.total)
+                    is FileKitPickerState.Progress ->
+                        FileKitPickerState.Progress(processed = it.processed, total = it.total)
+
+                    is FileKitPickerState.Completed ->
+                        FileKitPickerState.Completed(result = it.result)
+                }
+            }
+        }
+
+        override suspend fun consumeResult(
+            result: Flow<FileKitPickerState<List<PlatformFile>>>,
+            onConsumed: (FileKitPickerState<List<PlatformFile>>) -> Unit
+        ) {
+            result.collect(onConsumed)
+        }
+    }
+}
+
+internal sealed class PickerMode {
+    data object Single : PickerMode()
+    data class Multiple(val maxItems: Int? = null) : PickerMode() {
         init {
             require(maxItems == null || maxItems in 1..50) {
                 "maxItems must be contained between 1 <= maxItems <= 50 but current value is $maxItems"
             }
-        }
-
-        override fun parseResult(value: List<PlatformFile>?): List<PlatformFile>? {
-            return value?.takeIf { it.isNotEmpty() }
         }
     }
 }
