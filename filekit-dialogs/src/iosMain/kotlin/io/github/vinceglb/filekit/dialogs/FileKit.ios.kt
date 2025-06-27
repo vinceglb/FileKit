ea428ec968275e5385bc7afaab2a623e6d728bc9
Vincent Guillebaud <vincent.guillebaud31@gmail.com>
2025-06-27 20:18:31 +0200

    🎨 Format code

--- filekit-dialogs/src/iosMain/kotlin/io/github/vinceglb/filekit/dialogs/FileKit.ios.kt
+++ filekit-dialogs/src/iosMain/kotlin/io/github/vinceglb/filekit/dialogs/FileKit.ios.kt
@@ -25,7 +25,6 @@ import platform.Foundation.NSData
 import platform.Foundation.NSFileManager
 import platform.Foundation.NSURL
 import platform.Foundation.NSUUID
-import platform.Foundation.dataWithContentsOfURL
 import platform.Foundation.temporaryDirectory
 import platform.Foundation.writeToURL
 import platform.Photos.PHPhotoLibrary.Companion.sharedPhotoLibrary
@@ -224,7 +223,7 @@ public actual suspend fun FileKit.shareFile(
 
     // Ensure we always pass a file URL to the activity items; otherwise iOS may treat the
     // provided value as plain text and share the path string instead of the actual file.
-    val shareItem = platform.Foundation.NSURL.fileURLWithPath(file.path)
+    val shareItem = NSURL.fileURLWithPath(file.path)
 
     val shareVC = UIActivityViewController(
         activityItems = listOf(shareItem),
