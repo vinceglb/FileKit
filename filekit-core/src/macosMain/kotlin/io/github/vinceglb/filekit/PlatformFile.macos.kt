06b8027c7ae472e9d33e1707cb55d6945c7c1da2
N7ghtm4r3 <maurizio.manuel2003@gmail.com>
2025-07-26 21:15:39 +0200

    Integrated the open method on PlatformFile.macos.kt

--- filekit-core/src/macosMain/kotlin/io/github/vinceglb/filekit/PlatformFile.macos.kt
+++ filekit-core/src/macosMain/kotlin/io/github/vinceglb/filekit/PlatformFile.macos.kt
@@ -1,5 +1,20 @@
 package io.github.vinceglb.filekit
 
-public actual fun PlatformFile.open() {
+import platform.AppKit.NSWorkspace
+import platform.AppKit.openFile
+import platform.Foundation.NSFileManager
 
+public actual fun PlatformFile.open() {
+    val fileManager = NSFileManager.defaultManager
+    val workspace = NSWorkspace.sharedWorkspace
+    val absolutePath = absolutePath()
+    if(fileManager.fileExistsAtPath(absolutePath)) {
+        workspace.openFile(
+            fullPath = absolutePath
+        )
+    } else {
+        workspace.openURL(
+            url = nsUrl
+        )
+    }
 }
