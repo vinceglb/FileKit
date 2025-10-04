1c9ab2448213e9db690158d0323286aca7449412
Vincent Guillebaud <vincent.guillebaud31@gmail.com>
2025-10-04 20:15:18 +0200

    ✅ Add @Serialization tests

--- filekit-core/src/wasmJsTest/kotlin/io/github/vinceglb/filekit/utils/CreateTestFile.wasmJs.kt
+++ filekit-core/src/wasmJsTest/kotlin/io/github/vinceglb/filekit/utils/CreateTestFile.wasmJs.kt
@@ -2,12 +2,14 @@ package io.github.vinceglb.filekit.utils
 
 import io.github.vinceglb.filekit.PlatformFile
 import org.w3c.files.File
+import org.w3c.files.FilePropertyBag
 
+@OptIn(ExperimentalWasmJsInterop::class)
 actual fun createTestFile(
     name: String,
     content: String
 ): PlatformFile {
     val jsArray = content.encodeToByteArray().toJsArray()
-    val file = File(jsArray, name)
+    val file = File(jsArray, name, FilePropertyBag(type = "text/plain"))
     return PlatformFile(file)
 }
