b285241d9510f3241acd7a414d71544613d6adb1
vinceglb <vincent.guillebaud31@gmail.com>
2025-11-30 03:00:35 +0100

    ♻️ Refactor Gradle configuration for Kotlin Multiplatform and update dependencies

--- filekit-coil/build.gradle.kts
+++ filekit-coil/build.gradle.kts
@@ -1,92 +1,17 @@
-import org.jetbrains.kotlin.gradle.ExperimentalWasmDsl
-import org.jetbrains.kotlin.gradle.dsl.JvmTarget
-
 plugins {
-    alias(libs.plugins.androidLibrary)
-    alias(libs.plugins.kotlinMultiplatform)
-    alias(libs.plugins.composeMultiplatform)
-    alias(libs.plugins.composeCompiler)
+    alias(libs.plugins.filekit.composeMultiplatformLibrary)
     alias(libs.plugins.vanniktech.mavenPublish)
 }
 
 kotlin {
-    explicitApi()
-
-    // https://kotlinlang.org/docs/multiplatform-hierarchy.html#creating-additional-source-sets
-    applyDefaultHierarchyTemplate()
-
-    // Android
-    androidTarget {
-        publishLibraryVariants("release")
-        compilerOptions {
-            jvmTarget.set(JvmTarget.JVM_11)
-        }
-    }
-
-    // JVM / Desktop
-    jvm()
-
-    // JS / Web
-    @OptIn(ExperimentalWasmDsl::class)
-    listOf(
-        js(),
-        wasmJs(),
-    ).forEach {
-        it.outputModuleName = "FileKitCoil"
-        it.browser()
-    }
-
-    // iOS
-    listOf(
-        iosX64(),
-        iosArm64(),
-        iosSimulatorArm64(),
-    ).forEach {
-        it.binaries.framework {
-            baseName = "FileKitCoil"
-            isStatic = true
-        }
-    }
-
     sourceSets {
         commonMain.dependencies {
-            // Compose
-            implementation(compose.runtime)
-
-            // Coil
             api(libs.coil.compose)
-
-            // FileKit
             api(projects.filekitCore)
         }
 
-        val nonWebMain by creating {
-            dependsOn(commonMain.get())
-        }
-
         webMain.dependencies {
-            // Coroutines
             implementation(libs.kotlinx.coroutines.core)
         }
-
-        androidMain.get().dependsOn(nonWebMain)
-        jvmMain.get().dependsOn(nonWebMain)
-        nativeMain.get().dependsOn(nonWebMain)
-    }
-
-    compilerOptions {
-        freeCompilerArgs.add("-Xexpect-actual-classes")
-    }
-}
-
-android {
-    namespace = "io.github.vinceglb.filekit.coil"
-    compileSdk = libs.versions.android.compileSdk.get().toInt()
-    defaultConfig {
-        minSdk = libs.versions.android.minSdk.get().toInt()
-    }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
     }
 }
--- filekit-core/build.gradle.kts
+++ filekit-core/build.gradle.kts
@@ -1,130 +1,43 @@
-import org.jetbrains.kotlin.gradle.ExperimentalWasmDsl
-import org.jetbrains.kotlin.gradle.dsl.JvmTarget
-
 plugins {
-    alias(libs.plugins.androidLibrary)
-    alias(libs.plugins.kotlinMultiplatform)
+    alias(libs.plugins.filekit.kotlinMultiplatformLibrary)
     alias(libs.plugins.kotlinSerialization)
     alias(libs.plugins.vanniktech.mavenPublish)
 }
 
 kotlin {
-    explicitApi()
-
-    // https://kotlinlang.org/docs/multiplatform-hierarchy.html#creating-additional-source-sets
-    applyDefaultHierarchyTemplate()
-
-    // Android
-    androidTarget {
-        publishLibraryVariants("release")
-        compilerOptions {
-            jvmTarget.set(JvmTarget.JVM_11)
-        }
-    }
-
-    // JVM / Desktop
-    jvm()
-
-    // JS / Web
-    @OptIn(ExperimentalWasmDsl::class)
-    listOf(
-        js(IR),
-        wasmJs(),
-    ).forEach {
-        it.outputModuleName = "FileKit"
-        it.browser()
-    }
-
-    // iOS / macOS
-    listOf(
-        iosX64(),
-        iosArm64(),
-        iosSimulatorArm64(),
-        macosX64(),
-        macosArm64(),
-    ).forEach { iosTarget ->
-        iosTarget.binaries.framework {
-            baseName = "FileKit"
-            isStatic = true
-        }
-    }
-
     sourceSets {
+        val jvmAndNativeMain by creating { dependsOn(nonWebMain.get()) }
+        jvmMain.get().dependsOn(jvmAndNativeMain)
+        nativeMain.get().dependsOn(jvmAndNativeMain)
+
         commonMain.dependencies {
-            // Coroutines
-            implementation(libs.kotlinx.coroutines.core)
-
-            // Kotlinx IO
             api(libs.kotlinx.io)
-
-            // Kotlinx Serialization
+            implementation(libs.kotlinx.coroutines.core)
             implementation(libs.kotlinx.serialization.core)
         }
 
-        commonTest.dependencies {
-            implementation(libs.kotlin.test)
-            implementation(libs.kotlinx.coroutines.test)
+        nonWebMain.dependencies {
+            implementation(libs.androidx.annotation)
         }
-
-        val nonWebMain by creating {
-            dependsOn(commonMain.get())
-            dependencies {
-                implementation(libs.androidx.annotation)
-            }
-        }
-        val nonWebTest by creating { dependsOn(commonTest.get()) }
-        nonWebTest.dependencies {
-            implementation(libs.kotlinx.serialization.json)
-        }
-
-        val jvmAndNativeMain by creating { dependsOn(nonWebMain) }
 
         androidMain {
-            dependsOn(nonWebMain)
             dependencies {
                 implementation(libs.androidx.documentfile)
                 implementation(libs.androidx.startup)
                 implementation(libs.androidx.exifinterface)
             }
         }
-        androidUnitTest {
-            dependsOn(nonWebTest)
-            dependencies {
-                implementation(libs.test.android.robolectric)
-            }
-        }
-        jvmMain.get().dependsOn(jvmAndNativeMain)
-        jvmTest.get().dependsOn(nonWebTest)
-        nativeMain.get().dependsOn(jvmAndNativeMain)
-        nativeTest.get().dependsOn(nonWebTest)
 
         wasmJsMain.dependencies {
             implementation(libs.kotlinx.browser)
         }
-    }
 
-    compilerOptions {
-        freeCompilerArgs.add("-Xexpect-actual-classes")
-    }
-}
+        androidHostTest.dependencies {
+            implementation(libs.test.android.robolectric)
+        }
 
-android {
-    namespace = "io.github.vinceglb.filekit"
-    compileSdk = libs.versions.android.compileSdk.get().toInt()
-    defaultConfig {
-        minSdk = libs.versions.android.minSdk.get().toInt()
+        nonWebTest.dependencies {
+            implementation(libs.kotlinx.serialization.json)
+        }
     }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
-    }
-}
-
-tasks.register<Copy>("copyTestResourcesToIos") {
-    from("src/nonWebTest/resources")
-    into("build/bin/iosSimulatorArm64/debugTest/src/nonWebTest/resources")
-}
-
-tasks.named("iosSimulatorArm64Test") {
-    dependsOn("copyTestResourcesToIos")
 }
