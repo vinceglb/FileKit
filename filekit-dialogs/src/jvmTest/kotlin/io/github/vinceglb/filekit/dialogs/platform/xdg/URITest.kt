9a3a097af14fa142b054131ca9b9b4749145acb9
Vincent Guillebaud <vincent.guillebaud31@gmail.com>
2025-11-30 15:02:26 +0100

    🧪 Fix file path assertions in URITest for Windows compatibility

--- filekit-dialogs/src/jvmTest/kotlin/io/github/vinceglb/filekit/dialogs/platform/xdg/URITest.kt
+++ filekit-dialogs/src/jvmTest/kotlin/io/github/vinceglb/filekit/dialogs/platform/xdg/URITest.kt
@@ -1,5 +1,6 @@
 package io.github.vinceglb.filekit.dialogs.platform.xdg
 
+import io.github.vinceglb.filekit.utils.Platform
 import java.io.File
 import kotlin.test.Test
 import kotlin.test.assertEquals
@@ -10,15 +11,16 @@ class URITest {
         val path = "file:///home/user/file.txt"
         val uri = path.toURI()
         val file = File(uri)
-        assertEquals("/home/user/file.txt", file.path)
+        assertEquals("/home/user/file.txt".replace("/", File.separator), file.path)
     }
 
     @Test
     fun testURIWithSpaces() {
+        Platform.Windows
         val path = "file:///home/user/file with spaces.txt"
         val uri = path.toURI()
         val file = File(uri)
-        assertEquals("/home/user/file with spaces.txt", file.path)
+        assertEquals("/home/user/file with spaces.txt".replace("/", File.separator), file.path)
     }
 
     @Test
@@ -26,7 +28,7 @@ class URITest {
         val path = "file:///home/user/Ubuntu [24.04].file"
         val uri = path.toURI()
         val file = File(uri)
-        assertEquals("/home/user/Ubuntu [24.04].file", file.path)
+        assertEquals("/home/user/Ubuntu [24.04].file".replace("/", File.separator), file.path)
     }
 
     @Test
@@ -34,6 +36,6 @@ class URITest {
         val path = "file:///home/dik/%D0%A0%D0%B0%D0%B1%D0%BE%D1%87%D0%B8%D0%B9%20%D1%81%D1%82%D0%BE%D0%BB/Ubuntu%20[24.04].torrent"
         val uri = path.toURI()
         val file = File(uri)
-        assertEquals("/home/dik/Рабочий стол/Ubuntu [24.04].torrent", file.path)
+        assertEquals("/home/dik/Рабочий стол/Ubuntu [24.04].torrent".replace("/", File.separator), file.path)
     }
 }
