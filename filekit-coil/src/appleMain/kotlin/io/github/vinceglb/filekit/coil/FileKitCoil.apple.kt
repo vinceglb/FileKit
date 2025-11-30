ba97186c072e5d83ded34955d7c7abca78f3dfc1
vinceglb <vincent.guillebaud31@gmail.com>
2025-11-30 17:47:59 +0100

    📝 Add KDoc for filekit-coil module

--- filekit-coil/src/appleMain/kotlin/io/github/vinceglb/filekit/coil/FileKitCoil.apple.kt
+++ filekit-coil/src/appleMain/kotlin/io/github/vinceglb/filekit/coil/FileKitCoil.apple.kt
@@ -2,5 +2,8 @@ package io.github.vinceglb.filekit.coil
 
 import io.github.vinceglb.filekit.PlatformFile
 
+/**
+ * Returns the underlying [NSURL] object for Apple platforms.
+ */
 internal actual val PlatformFile.underlyingFile: Any
     get() = nsUrl
--- filekit-coil/src/jvmMain/kotlin/io/github/vinceglb/filekit/coil/FileKitCoil.jvm.kt
+++ filekit-coil/src/jvmMain/kotlin/io/github/vinceglb/filekit/coil/FileKitCoil.jvm.kt
@@ -2,5 +2,8 @@ package io.github.vinceglb.filekit.coil
 
 import io.github.vinceglb.filekit.PlatformFile
 
+/**
+ * Returns the underlying [java.io.File] object for JVM.
+ */
 internal actual val PlatformFile.underlyingFile: Any
     get() = file
