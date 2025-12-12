d5c4311244890a153ba17c67145b9819140279b8
vinceglb <vincent.guillebaud31@gmail.com>
2025-12-12 02:31:45 +0100

    🎨 Add theme support with color and typography definitions

---
+++ sample/composeApp/src/commonMain/kotlin/io/github/vinceglb/filekit/sample/theme/Color.kt
@@ -0,0 +1,11 @@
+package io.github.vinceglb.filekit.sample.theme
+
+import androidx.compose.ui.graphics.Color
+
+internal val Purple80 = Color(0xFFD0BCFF)
+internal val PurpleGrey80 = Color(0xFFCCC2DC)
+internal val Pink80 = Color(0xFFEFB8C8)
+
+internal val Purple40 = Color(0xFF6650A4)
+internal val PurpleGrey40 = Color(0xFF625B71)
+internal val Pink40 = Color(0xFF7D5260)
