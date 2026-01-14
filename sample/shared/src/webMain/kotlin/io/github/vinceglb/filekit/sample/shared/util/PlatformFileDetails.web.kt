bda0479c099720f8cc37313b3ff33f44a9326660
vinceglb <vincent.guillebaud31@gmail.com>
2026-01-14 01:26:11 +0100

    🔥 Clean old files

--- sample/shared/src/webMain/kotlin/io/github/vinceglb/filekit/sample/shared/util/PlatformFileDetails.web.kt
+++
@@ -1,5 +0,0 @@
-package io.github.vinceglb.filekit.sample.shared.util
-
-import io.github.vinceglb.filekit.PlatformFile
-
-internal actual fun PlatformFile.platformDetailSections(): List<FileDetailSection> = emptyList()
